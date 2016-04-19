461918bb5ae7b71229398be4b47bd3c6f1ecfb1a
Benjamin Hendrich <bh@gastro-instruments.de>
2016-04-19 14:36:34 +0200

    perf(): optimizations

--- span/index.js
+++ span/index.js
@@ -2,30 +2,26 @@
 
 module.exports = class Span {
 	constructor(from, to) {
-		this.from = from;
-		this.to = to;
+		this.from = Number(from);
+		this.to = Number(to);
 	}
 
 	contains(date) {
-		return (this.from <= date) && (date <= this.to);
+		return this.from <= date && date <= this.to;
 	}
 
 	inside(date) {
-		return (this.from < date) && (date < this.to);
+		return this.from < date && date < this.to;
 	}
 
 	overlaps(span) {
-		return (
-			(this.from < span.from && span.from < this.to) ||
-			(this.from < span.to && span.to < this.to) ||
-			(span.from <= this.from && this.to <= span.to)
-		);
+		return this.to > Number(span.from) && this.from < Number(span.end);
 	}
 
 	intersection(span) {
 		return (new Span(
-			new Date(Math.min(this.from, span.from)),
-			new Date(Math.max(this.to, span.to))
+			Math.min(this.from, span.from),
+			Math.max(this.to, span.to)
 		));
 	}
 };
