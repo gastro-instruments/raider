7362ecdeb483f4fa3cfec17a65822f24f29acf98
Benjamin Hendrich <bh@gastro-instruments.de>
2016-04-20 13:45:45 +0200

    perf(): add benches for span.xor and span.getMinutes

--- span/bench.js
+++ span/bench.js
@@ -137,3 +137,86 @@ suite('intersection', function () {
 		twixSpan.intersection(twixAfter);
 	});
 });
+
+suite('xor', function () {
+	const dateFrom = new Date(2010, 0, 2, 0, 0, 0, 0);
+	const dateTo = new Date(2010, 0, 2, 8, 0, 0, 0);
+
+	const dates = [
+		new Date(2010, 0, 1, 0, 0, 0, 0),
+		new Date(2010, 0, 2, 2, 0, 0, 0),
+		new Date(2010, 0, 2, 6, 0, 0, 0),
+		new Date(2010, 0, 3, 0, 0, 0, 0),
+		new Date(2010, 0, 3, 6, 0, 0, 0)
+	]
+
+	const raiderSpan = new Span(dateFrom, dateTo);
+	const raiderIn = new Span(dates[1], dates[2]);
+	const raiderOutside = new Span(dates[3], dates[4]);
+	const raiderBefore = new Span(dates[0], dates[1]);
+	const raiderAfter = new Span(dates[2], dates[3]);
+
+	const twixSpan = moment.twix(dateFrom, dateTo);
+	const twixIn = moment.twix(dates[1], dates[2]);
+	const twixOutside = moment.twix(dates[3], dates[4]);
+	const twixBefore = moment.twix(dates[0], dates[1]);
+	const twixAfter = moment.twix(dates[2], dates[3]);
+
+	bench('raider - engulfed', function () {
+		raiderSpan.xor(raiderIn);
+	});
+
+	bench('raider - outside', function () {
+		raiderSpan.xor(raiderOutside);
+	});
+
+	bench('raider - before', function () {
+		raiderSpan.xor(raiderBefore);
+	});
+
+	bench('raider - after', function () {
+		raiderSpan.xor(raiderAfter);
+	});
+
+	bench('twix - engulfed', function () {
+		twixSpan.xor(twixIn);
+	});
+
+	bench('twix - outside', function () {
+		twixSpan.xor(twixOutside);
+	});
+
+	bench('twix - before', function () {
+		twixSpan.xor(twixBefore);
+	});
+
+	bench('twix - after', function () {
+		twixSpan.xor(twixAfter);
+	});
+});
+
+suite('getMinutes', function () {
+	const date1 = new Date(2010, 0, 1, 0, 0, 0, 0);
+	const date2 = new Date(2010, 0, 1, 1, 0, 0, 0);
+	const date3 = new Date(2020, 0, 1, 0, 0, 0, 0);
+	const raiderSmall = new Span(date1, date2);
+	const raiderBig = new Span(date1, date3);
+	const twixSmall = moment.twix(date1, date2);
+	const twixBig = moment.twix(date1, date3);
+
+	bench('raider - small', function () {
+		raiderSmall.getMinutes();
+	});
+
+	bench('raider - big', function () {
+		raiderBig.getMinutes();
+	});
+
+	bench('twix - small', function () {
+		twixSmall.length('minutes');
+	});
+
+	bench('twix - big', function () {
+		twixBig.length('minutes');
+	});
+});
